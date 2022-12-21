dc698f0c95c72afdc922e124b94c6d04f44725c0
Pavel9474 <113004970+Pavel9474@users.noreply.github.com>
2022-12-21 19:41:24 +0500

    task1completed

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -2,9 +2,21 @@
 # 1 –> Понедельник
 # 10 –> Нет такого дня
 # 7 –> Воскресение
-day=input('Введите номер дня недели')
+day=int(input('Введите номер дня недели '))
+print(day)
 if day==1:
     print('Понедельник')
 elif day==2:
     print('Вторник')
-
+elif day==3:
+    print('Среда')
+elif day==4:
+    print('Четверг')
+elif day==5:
+    print('Пятница')
+elif day==6:
+    print('Суббота')
+elif day==7:
+    print('Воскресенье')
+else:
+    print('Нет такого дня')
